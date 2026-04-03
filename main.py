d51a40d61868a73e19cc30d21ff0acacf4ee24e6
jasperthefish <vrithikavaithi@gmail.com>
2026-04-03 14:37:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+# longest consecutive ones
+
 num = int(input("Enter an integer: "))
 
 count = 0       
-
 temp = num
+lst = []
 
 while temp:
     if temp & 1:
-        count += 1
-    temp >>= 1
+        count +=1
+        temp >>= 1
+    else:
+        lst.append(count)
+        count = 0
+        temp >>= 1
+print(lst)
 
-print(f"number of ones: {count}")
+# print(f"number of consecutive ones: {count}")
+
+
+# while temp:
+#     if temp & 1:
+#         count += 1
+#     temp >>= 1
+#     if count >=1:
+#         if count == count:
+#             break
